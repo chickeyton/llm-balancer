b013b07ebe312c146cd6adf8b1e459a9ee022021
chickeyton <ngton2014@gmail.com>
2025-11-18 12:07:27 +0800

    enhancement
    Signed-off-by: chickeyton <ngton2014@gmail.com>

--- llm_balancer/api/http/pipeline/utils.py
+++ llm_balancer/api/http/pipeline/utils.py
@@ -37,16 +37,18 @@ async def async_send_task(request_json, task_handle):
             logprobs=True,
             max_tokens=1 if task_handle.stage == Stage.PREFILL else request_json.get("max_tokens")
         )
-        print(f"====== before yield stream.headers:{stream.response.headers}, stream.status_code:{stream.response.status_code}")
         yield stream.response.headers, stream.response.status_code
-        print(f"====== after yield stream.headers, stream.status_code")
         for chunk in stream:
 
             print(f"======= chunk: {chunk}")
-
-            response_text += chunk.choices[0].delta.content
-            chunk_len = len(chunk.choices[0].delta.logprobs.contents)
-            task_handle.on_respond(chunk_len)
+            choice = chunk.choices[0]
+            response_text += choice.delta.content
+            if choice.logprobs and choice.logprobs.contents:
+                chunk_len = len(choice.logprobs.contents)
+            else:
+                chunk_len = 0
+            if chunk_len > 0:
+                task_handle.on_respond(chunk_len)
             stream_data = chunk.model_dump_json()
             yield f"data: {stream_data}\n\n"
         task_handle.on_finished()
