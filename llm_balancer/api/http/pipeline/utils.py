1e99c87d97731bf172edc686562b14e1182a1059
chickeyton <ngton2014@gmail.com>
2025-11-18 16:02:27 +0800

    enhancement
    Signed-off-by: chickeyton <ngton2014@gmail.com>

--- llm_balancer/api/http/pipeline/utils.py
+++ llm_balancer/api/http/pipeline/utils.py
@@ -54,7 +54,7 @@ async def async_send_task(request_json, task_handle):
             #    print(f"=============== {len(choice.token_ids)}")
             #    task_handle.on_respond(len(choice.token_ids))
             print(f"===========  {choice}")
-            print(f"===========  {choice.delta.token_ids}")
+            print(f"===========  {dir(choice.delta)}")
             if not request_json.get("logprobs"):
                 choice.logprobs = None
             stream_data = chunk.model_dump_json()
