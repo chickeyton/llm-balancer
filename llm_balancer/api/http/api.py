6d93f4913bbe1a99e7eb0a9924d1d077dd79f212
chickeyton <ngton2014@gmail.com>
2025-11-18 11:17:45 +0800

    enhancement
    Signed-off-by: chickeyton <ngton2014@gmail.com>

--- llm_balancer/api/http/api.py
+++ llm_balancer/api/http/api.py
@@ -7,7 +7,7 @@ api_router = APIRouter()
 @api_router.post("/v1/chat/completions")
 async def chat_completions(request: Request, background_tasks: BackgroundTasks):
     stream = request.app.state.pipeline.handle_chat_completions(request, background_tasks)
-    #headers, status = await anext(stream)
+    dump = await anext(stream)
     #headers_dict = dict(headers.items())
     return StreamingResponse(
         stream,
