108bc78dc0adf8d48d46704590473cecf5f9f569
chickeyton <ngton2014@gmail.com>
2025-11-18 10:36:18 +0800

    enhancement
    Signed-off-by: chickeyton <ngton2014@gmail.com>

--- llm_balancer/api/http/api.py
+++ llm_balancer/api/http/api.py
@@ -7,5 +7,5 @@ api_router = APIRouter()
 @api_router.post("/v1/chat/completions")
 async def chat_completions(request: Request, background_tasks: BackgroundTasks):
     return StreamingResponse(
-        request.app.state.pipline.handle_chat_completions(request, background_tasks),
+        request.app.state.pipeline.handle_chat_completions(request, background_tasks),
         media_type="text/event-stream")
