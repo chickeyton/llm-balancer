9e1c6e360036fe953a259cb4ba8eb244bcc48ed7
chickeyton <ngton2014@gmail.com>
2025-11-19 14:42:03 +0800

    enhancement
    Signed-off-by: chickeyton <ngton2014@gmail.com>

--- llm_balancer/api/http/pipeline/p_d.py
+++ llm_balancer/api/http/pipeline/p_d.py
@@ -16,7 +16,7 @@ class P_D_Pipeline(Pipeline):
             yield resp
         if handle.error:
             return
-        decode_task = to_decode_task(handle.route, -1)
+        decode_task = to_decode_task(handle.route, 100)
         handle = self._balancer.route(decode_task).on_submit()
         async for resp in async_send_stream_task(request_json, handle):
             yield resp
