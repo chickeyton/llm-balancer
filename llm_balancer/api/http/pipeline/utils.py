04cbff9a9ab9befd5bed64b99df840561b297c65
chickeyton <ngton2014@gmail.com>
2025-11-19 17:46:04 +0800

    enhancement
    Signed-off-by: chickeyton <ngton2014@gmail.com>

--- llm_balancer/api/http/pipeline/utils.py
+++ llm_balancer/api/http/pipeline/utils.py
@@ -157,10 +157,7 @@ async def async_send_stream_p_then_d(request_json, task_handle, yield_headers=Fa
             if hasattr(choice, "token_ids"):
                 # sometimes choice.token_ids doesn't not exists
                 chunk_len = len(choice.token_ids)
-            else:
-                chunk_len = 0
-            # print(f"========================= chunk_len: {chunk_len}")
-            if chunk_len > 0:
+                # print(f"========================= chunk_len: {chunk_len}")
                 task_handle.on_respond(chunk_len)
 
             stream_data = chunk.model_dump_json()
