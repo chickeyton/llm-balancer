66ef58db2e20f43963f52908149652cc17081ade
chickeyton <ngton2014@gmail.com>
2025-11-20 15:27:03 +0800

    add test
    Signed-off-by: chickeyton <ngton2014@gmail.com>

--- llm_balancer/api/http/pipeline/p_d.py
+++ llm_balancer/api/http/pipeline/p_d.py
@@ -11,7 +11,10 @@ class P_D_Pipeline(Pipeline):
     async def handle_chat_completions(self, request, _):
         request_json = await request.json()
         prefill_task = to_prefill_task(self._tokenizer, request, request_json)
-        self._balancer.dynamic_pd.update()
+        advice = self._balancer.dynamic_pd.update(advice_only=False)
+        # if need to re-balance P/D ratio by external mechanisms, then set advice_only = True
+        # and obtain the suggested endpoint and stage by advice.best_switchable, advice.switchables
+        # and advice.new_stage
         handle = self._balancer.route(prefill_task).on_submit()
         print(f"Send prefill -> {handle.endpoint.id}")
         await async_send_prefill(request_json, handle)
--- llm_balancer/api/http/pipeline/pd.py
+++ llm_balancer/api/http/pipeline/pd.py
@@ -11,7 +11,10 @@ class PD_Pipeline(Pipeline):
     async def handle_chat_completions(self, request, _):
         request_json = await request.json()
         task = to_prefill_then_decode_task(self._tokenizer, request, request_json)
-        self._balancer.dynamic_pd.update()
+        advice = self._balancer.dynamic_pd.update(advice_only=False)
+        # if need to re-balance P/D ratio by external mechanisms, then set advice_only = True
+        # and obtain the suggested endpoint and stage by advice.best_switchable, advice.switchables
+        # and advice.new_stage
         handle = self._balancer.route(task).on_submit()
         print(f"Send prefill then decode -> {handle.endpoint.id}")
         async for resp in async_send_stream_p_then_d(request_json, handle, yield_headers=True):
