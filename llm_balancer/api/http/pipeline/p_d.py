c998f7fd3b3ad95f65563adc4439fff91becbdb1
chickeyton <ngton2014@gmail.com>
2025-11-20 15:30:52 +0800

    add test
    Signed-off-by: chickeyton <ngton2014@gmail.com>

--- llm_balancer/api/http/pipeline/p_d.py
+++ llm_balancer/api/http/pipeline/p_d.py
@@ -14,7 +14,8 @@ class P_D_Pipeline(Pipeline):
         advice = self._balancer.dynamic_pd.update(advice_only=False)
         # if need to re-balance P/D ratio by external mechanisms, then set advice_only = True
         # and obtain the suggested endpoint and stage by advice.best_switchable, advice.switchables
-        # and advice.new_stage
+        # and advice.new_stage, if advice is None means there is not enough stats or time for
+        # advisory
         handle = self._balancer.route(prefill_task).on_submit()
         print(f"Send prefill -> {handle.endpoint.id}")
         await async_send_prefill(request_json, handle)
