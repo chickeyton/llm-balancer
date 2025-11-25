78cfab69072414931b06973bf5dcfa6f59e83ee0
chickeyton <ngton2014@gmail.com>
2025-11-25 16:54:49 +0800

    enhancement
    Signed-off-by: chickeyton <ngton2014@gmail.com>

--- llm_balancer/api/http/pipeline/pd.py
+++ llm_balancer/api/http/pipeline/pd.py
@@ -11,7 +11,6 @@ class PD_Pipeline(Pipeline):
     async def handle_chat_completions(self, request, _):
         request_json = await request.json()
         task = to_prefill_then_decode_task(self._tokenizer, request, request_json)
-        self._balancer.dynamic_pd.update()
         handle = self._balancer.route(task).on_submit()
         print(f"Send prefill then decode -> {handle.endpoint.id}")
         async for resp in async_send_stream_p_then_d(request_json, handle, yield_headers=True):
