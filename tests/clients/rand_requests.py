b54e6293e4b4fb789581b8f7b1fd147c49551477
chickeyton <ngton2014@gmail.com>
2025-11-28 10:31:40 +0800

    enhancement
    Signed-off-by: chickeyton <ngton2014@gmail.com>

--- tests/clients/rand_requests.py
+++ tests/clients/rand_requests.py
@@ -13,8 +13,8 @@ num_workers = 50
 num_max_active_requests = 5000
 fixed_prefix_len = 0
 subfix_min_len = 30
-subfix_max_len = 1000
-max_tokens = 1230
+subfix_max_len = 3000
+max_tokens = 3230
 
 
 word_pool = ["hi", "hello", "yes", "no", "cat", "dog", "pig", "game", "coffee", "cake", "noodles", "burger", "football", "tennis", "ship", "car", "ship", "boat"]
