aa9205e4dfae267863ec1ef5a36006ad8fd40c82
chickeyton <ngton2014@gmail.com>
2025-11-28 11:46:32 +0800

    enhancement
    Signed-off-by: chickeyton <ngton2014@gmail.com>

--- tests/batch_routing/test_optimizer.py
+++ tests/batch_routing/test_optimizer.py
@@ -5,7 +5,8 @@ from llm_balancer.balancer.router import BatchRouteLocalSearch
 
 num_workers = 10
 num_tasks = 500
-task_workloads = np.random.uniform(1, 1000, size=(num_workers, num_tasks)).astype(np.float64)
+task_workloads = np.random.uniform(1, 1000, size=(1, num_tasks)).astype(np.float64)
+task_workloads = np.tile(task_workloads, (num_workers, 1))
 queue_workloads = np.random.uniform(0, 10000, size=(num_workers, )).astype(np.float64)
 #queue_workloads = np.zeros(num_workers, dtype=np.float64)
 
