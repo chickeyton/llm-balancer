5bbe8f8f94e7f21b919a8d914066273a96d80838
chickeyton <ngton2014@gmail.com>
2025-11-28 11:19:19 +0800

    enhancement
    Signed-off-by: chickeyton <ngton2014@gmail.com>

--- tests/batch_routing/test_optimizer.py
+++ tests/batch_routing/test_optimizer.py
@@ -1,17 +1,21 @@
+import time
+
 import numpy as np
 from llm_balancer.balancer.router import BatchRouteLocalSearch
 
-task_workloads = np.random.uniform(1, 1000, size=(10, 50))
-queue_workloads = np.random.uniform(0, 10000, size=(task_workloads.shape[0], ))
+task_workloads = np.random.uniform(1, 1000, size=(10, 50), dtype=np.float64)
+queue_workloads = np.random.uniform(0, 10000, size=(task_workloads.shape[0], ), dtype=np.float64)
 
 
+start = time.time()
 optimizer = BatchRouteLocalSearch()
 assign, worker_workloads = optimizer.optimize(task_workloads, queue_workloads)
-
+elpased = time.time() - start
 
 original_var = np.var(queue_workloads)
 new_var = np.var(worker_workloads)
 
+print(f"compute time: {elpased}")
 print(f"original variance:{original_var}")
 print(f"new variance:{new_var}")
 print(f"diff:{new_var - original_var}")
