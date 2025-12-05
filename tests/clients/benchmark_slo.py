cece4a7c2551383058eb1831126a2e9e1d699e54
chickeyton <ngton2014@gmail.com>
2025-12-05 10:03:45 +0800

    enhancement
    Signed-off-by: chickeyton <ngton2014@gmail.com>

--- tests/clients/benchmark_slo.py
+++ tests/clients/benchmark_slo.py
@@ -72,11 +72,11 @@ def request_proc(worker_id, io_remain_requests, o_ttfts, o_tpots, o_slo_passes):
 
         cut = np.random.uniform()
         if cut < 0.05:
-            subfix_len = np.random.randint(10000, 20000)
+            subfix_len = 15000 # np.random.randint(10000, 20000)
         elif cut < 0.3:
-            subfix_len = np.random.randint(100, 1000)
+            subfix_len = 1000 # np.random.randint(100, 1000)
         else:
-            subfix_len = np.random.randint(10, 100)
+            subfix_len = 100 # np.random.randint(10, 100)
 
         prompt2 = gen_prompt(subfix_len)
         ttft, tpot = http_request(fixed_prefix + ' ' + prompt2)
