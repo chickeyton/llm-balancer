de90bf447bd62e7ef5331318cd39cc19105cc86a
chickeyton <ngton2014@gmail.com>
2025-12-09 11:00:47 +0800

    enhancement
    Signed-off-by: chickeyton <ngton2014@gmail.com>

--- llm_balancer/api/http/config.py
+++ llm_balancer/api/http/config.py
@@ -86,9 +86,7 @@ def parse_app_config(json_dict) -> AppConfig:
         #config.lmcache.is_cache_shared = bool(lmcache_obj.get("is_cache_shared"))
         # print(f"lmcache_obj {int(lmcache_obj.get('controller_pull_port'))}, {int(lmcache_obj.get('controller_reply_port'))}")
 
-    print(f"lLLLLLLLLLLLLLLL {config.lmcache.contorller_pull_port}, {config.lmcache.contorller_reply_port}")
-
-
+    #print(f"lLLLLLLLLLLLLLLL {config.lmcache.contorller_pull_port}, {config.lmcache.contorller_reply_port}")
 
     batch_routing_obj = json_dict.get("batch_routing")
     if batch_routing_obj:
