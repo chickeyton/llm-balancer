13edc030de8eb21913cb2b14696f37df8266d5a8
chickeyton <ngton2014@gmail.com>
2025-12-10 15:16:37 +0800

    enhancement
    Signed-off-by: chickeyton <ngton2014@gmail.com>

--- tests/clients/benchmark_slo.py
+++ tests/clients/benchmark_slo.py
@@ -9,7 +9,7 @@ model = "Qwen/Qwen2-7B"
 
 num_requests = 300
 num_workers = 1
-fixed_prefix_len = 10000
+fixed_prefix_len = 2100
 num_fixed_prefixs = 10
 subfix_min_len = 20
 subfix_max_len = subfix_min_len + fixed_prefix_len
