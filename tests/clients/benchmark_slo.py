236eb5a9f74a4617144f598a52cc266ae90788e8
chickeyton <ngton2014@gmail.com>
2025-12-10 16:56:47 +0800

    enhancement
    Signed-off-by: chickeyton <ngton2014@gmail.com>

--- tests/clients/benchmark_slo.py
+++ tests/clients/benchmark_slo.py
@@ -1,3 +1,6 @@
+import asyncio
+from threading import Thread
+
 import numpy as np
 import time
 from multiprocessing import Process, Manager, Value
@@ -7,13 +10,14 @@ base_url = "http://localhost:8888/v1"
 api = "/chat/completions"
 model = "Qwen/Qwen2-7B"
 
-num_requests = 300
+num_requests = 30
 num_workers = 20
-fixed_prefix_len = 10000
+fixed_prefix_len = 100
 num_fixed_prefixs = 10
 subfix_min_len = 20
 subfix_max_len = subfix_min_len + fixed_prefix_len
 max_tokens = 1
+rps = 10
 
 slo_ttft = 1
 slo_tpot = 0.25
@@ -22,12 +26,79 @@ slo_tpot = 0.25
 word_pool = ["hi", "hello", "yes", "no", "cat", "dog", "pig", "game", "coffee", "cake", "noodles", "burger", "football", "tennis", "ship", "car", "ship", "boat"]
 
 
-
 def gen_prompt(num_words):
     if num_words == 0:
         return ""
     rand_nums = np.random.randint(0, len(word_pool), num_words)
     return " ".join([word_pool[n] for n in rand_nums])
+
+np.random.seed(996)
+fixed_prefixs = [gen_prompt(fixed_prefix_len) for _ in range(num_fixed_prefixs)]
+
+
+def create_request(prompt):
+    json_obj = {}
+    json_obj["model"] = model
+    json_obj["messages"] = [{"role": "user", "content": prompt}]
+    # json_obj["prompt"] = prompt
+    json_obj["max_tokens"] = max_tokens
+    # json_obj["extra_body"] = {"return_token_ids": True}
+    json_obj["stream"] = True
+
+    client = OpenAI(api_key="", base_url=base_url)
+    request = client.chat.completions.create(**json_obj)
+    return request
+
+
+sent_requests = 0
+gathered_requests = 0
+open_requests = []
+
+
+async def send_requests():
+    global num_requests
+    global sent_requests
+    global open_requests
+    while sent_requests < num_requests:
+        update_start = time.time()
+        num_update_requests = 0
+        while num_update_requests < rps:
+            prompt = fixed_prefixs[np.random.randint(len(fixed_prefixs))]
+            if subfix_min_len > 0:
+                subfix_len = np.random.randint(subfix_min_len, subfix_max_len)
+                prompt += " " + gen_prompt(subfix_len)
+            open_requests.append(create_request(prompt))
+            num_update_requests += 1
+            sent_requests += 1
+        elapsed = time.time() - update_start
+        if elapsed < 1.0:
+            sleep_time = 1.0 - elapsed
+            yield asyncio.sleep(sleep_time)
+
+
+async def gather_requests():
+    global num_requests
+    global gathered_requests
+    global open_requests
+    while gathered_requests < num_requests:
+        requests = open_requests
+        open_requests = []
+        if len(requests) == 0:
+            yield asyncio.sleep(0.1)
+            continue
+        await asyncio.gather(requests)
+        gathered_requests += len(requests)
+
+
+loop = asyncio.new_event_loop()
+thread = Thread(target=loop.run_until_complete)
+thread.start()
+asyncio.run_coroutine_threadsafe(gather_requests(), loop)
+asyncio.run_coroutine_threadsafe(send_requests(), loop)
+thread.join()
+
+"""
+
 
 
 def http_request(prompt):
@@ -61,7 +132,6 @@ def http_request(prompt):
     return ttft, tpot
 
 
-fixed_prefixs = [gen_prompt(fixed_prefix_len) for _ in range(num_fixed_prefixs)]
 
 
 def request_proc(worker_id, io_remain_requests, o_ttfts, o_tpots, o_slo_passes):
@@ -73,23 +143,7 @@ def request_proc(worker_id, io_remain_requests, o_ttfts, o_tpots, o_slo_passes):
                 break
             io_remain_requests.value = io_remain_requests.value - 1
 
-        """
-        cut = np.random.uniform()
-        if cut < 0.05:
-            subfix_len = 22345
-        elif cut < 0.1:
-            subfix_len = 12345
-        elif cut < 0.15:
-            subfix_len = 8765
-        elif cut < 0.3:
-            subfix_len = 2345
-        elif cut < 0.5:
-            subfix_len = 1234
-        elif cut < 0.7:
-            subfix_len = 321
-        else:
-            subfix_len = 20
-        """
+        
 
         msg = fixed_prefixs[np.random.randint(len(fixed_prefixs))]
 
@@ -136,3 +190,5 @@ print(f"mean tpots: {np.mean(tpots)}")
 print(f"p99 tpots: {np.quantile(tpots, 0.99)}")
 print(f"SLO attainment rate: {np.mean(slo_passes)}")
 print(f"RPS: {rps}")
+
+"""
