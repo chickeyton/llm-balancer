1e15da20ef403a2f92747ae5dc627dfc2976626b
chickeyton <ngton2014@gmail.com>
2025-12-30 17:22:05 +0800

    enhancement
    Signed-off-by: chickeyton <ngton2014@gmail.com>

--- tests/dynamic_pd/test_dynamic_pd2.py
+++ tests/dynamic_pd/test_dynamic_pd2.py
@@ -5,8 +5,8 @@ import numpy as np
 dynamic_pd = DynamicPd(slo_config=SloConfig(), stats_config=StatsConfig())
 
 new_reqs = 3000
-ttfts = np.random.uniform(0.5, 1.0, new_reqs)
-tpots = np.random.uniform(0.1, 0.12, new_reqs)
+ttfts = np.random.uniform(0.01, 0.02, new_reqs)
+tpots = np.random.uniform(0.01, 0.012, new_reqs)
 for ttft, tpot in zip(ttfts, tpots):
     dynamic_pd.on_request_finished(ttft, tpot)
 
